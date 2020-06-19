802da8d5ff00f244bea469edeae4e00ed7200da9
Scott Page <scott.page@indysoft.com>
2020-06-18 23:56:53 -0600

    Added link to index.html

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,6 @@ import * as path from 'path';
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '..', 'public'), { index: 'index.html' }));
-
-app.get('/imhere', (_, res) => {
-  return res.json({
-    data: 'Glad you made it!'
-  });
-});
+app.get('/imhere', (_, res) => res.json({ data: 'Glad you made it!' }));
 
 app.listen(30000, () => console.info('Started listening on port 30000'));
